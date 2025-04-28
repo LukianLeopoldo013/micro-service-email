a3579b68cf0c484a971b534ec1bd18736087ea3f
LukianLeopoldo013 <glaucioleopoldo6@gmail.com>
2025-04-28 20:07:28 -0300

    Create a crud in db layer

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -9,10 +9,10 @@ import (
 )
 
 type EmailEntry struct {
-	Id         int64
-	Email      string
-	ConfirmeAt *time.Time
-	OptOut     bool
+	Id          int64
+	Email       string
+	ConfirmedAt *time.Time
+	OptOut      bool
 }
 
 func TryCreate(db *sql.DB) {
@@ -51,5 +51,97 @@ func emailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 
 	t := time.Unix(confirmedAt, 0)
 
-	return &EmailEntry{Id: id, Email: email, ConfirmeAt: &t, OptOut: optOut}, nil
+	return &EmailEntry{Id: id, Email: email, ConfirmedAt: &t, OptOut: optOut}, nil
+}
+
+func CreateEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(
+		`
+		INSERT INTO emails(email, confirmed_at, opt_out)
+		VALUES(?, 0, FALSE)
+		`, email)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	rows, err := db.Query(`
+		SELECT id, email, confirmed_at, opt_out
+		FROM emails
+		WHERE email = ?`, email)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		return emailEntryFromRow(rows)
+	}
+	return nil, nil
+}
+
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	t := entry.ConfirmedAt.Unix()
+
+	_, err := db.Exec(`
+		INSERT INTO 
+		emails(email, confirmed_at, opt_out)
+		VALUES(?,?,?)
+		ON CONFLIT(email) DO UPDATE SET
+		confirmed_at=?
+		opt_out=?`, entry.Email, t, entry.OptOut, t, entry.OptOut)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func DeleteEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+	UPDATE emails
+	SET opt_out=true
+	WHERE email=?`, email)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+type GetEmailBatchQukeryParams struct {
+	Page  int
+	Count int
+}
+
+func GetEmailBatch(db *sql.DB, params GetEmailBatchQukeryParams) ([]EmailEntry, error) {
+	var empty []EmailEntry
+	rows, err := db.Query(`
+		SELECT id, email, confirmed_at, opt_out
+		FROM emails
+		WHERE opt_out = false
+		ORDER BY id ASC
+		LIMIT ? OFFSET ?`, params.Count, (params.Page-1)*params.Count)
+	if err != nil {
+		log.Println(err)
+		return empty, err
+	}
+
+	defer rows.Close()
+
+	emails := make([]EmailEntry, 0, params.Count)
+	for rows.Next() {
+		email, err := emailEntryFromRow(rows)
+		if err != nil {
+			return nil, err
+		}
+		emails = append(emails, *email)
+	}
+	return emails, nil
 }
